a0f9c77a52d92274b1805769684c676a3afe0552
Matheus-Novoa <65021130+Matheus-Novoa@users.noreply.github.com>
2024-11-03 11:05:00 -0300

    Refatoração

--- bot.py
+++ bot.py
@@ -4,10 +4,15 @@ from botcity.web.browsers.chrome import default_options
 from dados import *
 import os
 from unidecode import unidecode
-from pyautogui import alert, confirm
+from pyautogui import confirm
 from dotenv import load_dotenv
 from pywinauto.application import Application
 from pywinauto.findwindows import find_window
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By as sBy
+from selenium.common.exceptions import StaleElementReferenceException, ElementClickInterceptedException
 
 
 
@@ -55,14 +60,14 @@ def trazer_janela_para_frente(titulo):
         janela.set_focus()
         return janela
     except Exception as e:
-        print(f"Erro ao trazer a janela para frente: {e}")
+        # print(f"Erro ao trazer a janela para frente: {e}")
         return None
 
 
 def main():
 
-    data_geracao = '30062024'
-    mes = 'JUNHO'
+    data_geracao = '31082024'
+    mes = 'AGOSTO'
     
     ANO = '2024'
     
@@ -72,7 +77,7 @@ def main():
     bot = WebBot()
     bot.headless = False
     bot.browser = Browser.CHROME
-    bot.driver_path = os.getenv('BOT_DRIVER_PATH')
+    bot.driver_path = ChromeDriverManager().install()
     profile_path = os.getenv('PROFILE_PATH')
 
     def_options = default_options(
@@ -88,11 +93,13 @@ def main():
     
     entrar(bot)
 
-    aba_geracao = bot.find_element('//*[@id="nav"]/div[1]/a/img', By.XPATH)
-    aba_geracao.click()
+    # aba_geracao
+    bot.find_element('//*[@id="nav"]/div[1]/a/img', By.XPATH).click()
     bot.wait(1000)
-    campo_geracao = bot.find_element('//*[@id="MesReferenciaModalPanelSubview:formMesReferencia:dtCompetencia"]', By.XPATH)
-    campo_geracao.send_keys(data_geracao)
+    
+    # campo_geracao
+    bot.find_element('//*[@id="MesReferenciaModalPanelSubview:formMesReferencia:dtCompetencia"]',
+                                     By.XPATH).send_keys(data_geracao)
     bot.enter()
     bot.wait(3000)
     
@@ -100,24 +107,42 @@ def main():
         
         try:
             ################### CPF ###################
-            campo_cpf = bot.find_element('//*[@id="form:numDocumento"]', By.XPATH)
-            campo_cpf.click()
-            campo_cpf.send_keys(cliente.CPF)
-            botao_lupa = bot.find_element('//*[@id="form:btAutoCompleteTomador"]', By.XPATH)
-            botao_lupa.click()
-            
-            if bot.find("erro_cpf_nao_encontrado", matching=0.97, waiting_time=1500):
-                not_found("erro_cpf_nao_encontrado")
-                alert(title='CPF não cadastrado. Preencha manualmente')
+            while True:
+                try:
+                    wait = WebDriverWait(bot.driver, 15)
+                    campo_cpf = wait.until(EC.element_to_be_clickable((sBy.XPATH, '//*[@id="form:numDocumento"]')))
+                    campo_cpf.click()
+                    campo_cpf.send_keys(cliente.CPF)
+                    # botao_lupa
+                    bot.find_element('//*[@id="form:btAutoCompleteTomador"]', By.XPATH).click()
+                    break
+                except ElementClickInterceptedException:
+                    continue
+            # if bot.find("erro_cpf_nao_encontrado", matching=0.97, waiting_time=1500):
+            #     not_found("erro_cpf_nao_encontrado")
+            #     alert(title='CPF não cadastrado. Preencha manualmente')
 
             nome_responsavel_dados = unidecode(cliente.ResponsávelFinanceiro.upper().strip())
-            campo_razao_social = bot.find_element('//*[@id="form:dnomeRazaoSocial"]', By.XPATH)
-            bot.wait_for_element_visibility(campo_razao_social)
-            nome_responsavel_retornado = unidecode(campo_razao_social.get_attribute('value').upper().strip())
+            campo_razao_social = bot.find_element('//*[@id="form:dnomeRazaoSocial"]',
+                                                    By.XPATH)
+
+            nome_responsavel_retornado = None
+            while not (nome_responsavel_retornado and campo_razao_social):
+                campo_razao_social = bot.find_element('//*[@id="form:dnomeRazaoSocial"]',
+                                                    By.XPATH)
+                try:
+                    nome_responsavel_retornado = unidecode(
+                        campo_razao_social.get_attribute('value').upper().strip())
+                except StaleElementReferenceException as err:
+                    print('Não foi possível realizar a leitura do nome')
+                    continue
 
             while nome_responsavel_retornado != nome_responsavel_dados:
-                resposta = confirm(text=f'{nome_responsavel_retornado} | {nome_responsavel_dados}',
-                        title='Nome do responsável não correspondente', buttons=['Continuar', 'Interromper'])
+                resposta = confirm(
+                        text=f'{nome_responsavel_retornado} | {nome_responsavel_dados}',
+                        title='Nome do responsável não correspondente',
+                        buttons=['Continuar', 'Interromper']
+                        )
                 if resposta == 'Continuar':
                     break
                 else:
@@ -127,44 +152,50 @@ def main():
             bot.wait_for_element_visibility(aba_identificacao_servico)
             aba_identificacao_servico.click()
             texto_descricao = f'PRESTAÇÃO DE SERVIÇO EDUCAÇÃO INFANTIL/FUNDAMENTAL MÊS {mes}/{ANO} - ALUNO {cliente.Aluno}'
-            campo_descricao = bot.find_element('//*[@id="form:descriminacaoServico"]', By.XPATH)
-            campo_descricao.send_keys(texto_descricao)
+            
+            # campo_descricao
+            bot.find_element('//*[@id="form:descriminacaoServico"]', By.XPATH).send_keys(texto_descricao)
 
             if cliente.Acumulador == '1':
-                opcao_turma = bot.find_element('//*[@id="form:codigoCnae"]', By.XPATH)
-                opcao_turma.click()
+                # opcao_turma
+                bot.find_element('//*[@id="form:codigoCnae"]', By.XPATH).click()
                 bot.type_up()
                 bot.enter()
 
             ################### VALOR ###################
-            # bot.wait(1000)
-            aba_valores = bot.find_element('//*[@id="topo_aba3"]/a', By.XPATH)
-            aba_valores.click()
-            campo_valor_total = bot.find_element('//*[@id="form:valorServicos"]', By.XPATH)
-            campo_valor_total.send_keys(cliente.ValorTotal)
+            # aba_valores
+            bot.find_element('//*[@id="topo_aba3"]/a', By.XPATH).click()
+            # campo_valor_total
+            bot.find_element('//*[@id="form:valorServicos"]', By.XPATH).send_keys(cliente.ValorTotal)
 
             ################### DOWNLOAD NOTA ###################
-            botao_gerar_nfs = bot.find_element('//*[@id="form:bt_emitir_NFS-e"]', By.XPATH)
-            botao_gerar_nfs.click()
-            bot.wait(2000)
+            # botao_gerar_nfs
+            bot.find_element('//*[@id="form:bt_emitir_NFS-e"]', By.XPATH).click()
+            # botao_confirmar_geracao
+            bot.find_element('//*[@id="appletAssinador:j_id766"]',
+                                By.XPATH,
+                                ensure_clickable=True).click()
             
-            botao_confirmar_geracao = bot.find_element('//*[@id="appletAssinador:j_id766"]', By.XPATH)
-            botao_confirmar_geracao.click()
-            
-            bot.wait(1000)
-
             nome_janela = 'Logon do Token'
+            bot.wait(500)
             janela = trazer_janela_para_frente(nome_janela)
             while janela == None:
                 janela = trazer_janela_para_frente(nome_janela)
+                bot.wait(500)
 
             bot_desktop.kb_type('123456')
             bot_desktop.enter()
-            bot.wait(3000)
             
-            botao_download = bot.find_element('//*[@id="form"]/input[2]', By.XPATH, ensure_clickable=True, ensure_visible=True)
-            # bot.wait_for_element_visibility(botao_download)
-            botao_download.click()
+            while True:
+                try:
+                    bot.wait(500)
+                    botao_download = wait.until(EC.element_to_be_clickable((sBy.XPATH, '//*[@id="form"]/input[2]')))
+                    botao_download.click()
+                    break
+                except Exception as err:
+                    print('Esperando o carregamento da página de downloads...')
+                    continue
+
             bot.wait(1000)
             num_nota = bot.get_last_created_file(download_folder_path).split(os.sep)[-1].split('.')[0][-4:]
 
@@ -176,18 +207,29 @@ def main():
                 f.write(f'Erro {cliente.ResponsávelFinanceiro} linha {cliente.Index}')
                 raise
         ################### RETORNA E LIMPA OS CAMPOS ###################
-        try:    
-            botao_retorno = bot.find_element('//*[@id="form:j_id9"]', By.XPATH)
-            botao_retorno.click()
-            bot.wait(3000)
-            
-            botao_limpar_digitacao = bot.find_element('//*[@id="form"]/input[3]', By.XPATH)
-            botao_limpar_digitacao.click()
-            bot.wait(2000)
-        except:
-            with open(arquivo_progresso, 'w') as f:
-                f.write(f'Erro após {cliente.ResponsávelFinanceiro} linha {int(cliente.Index) + 1}')
-                raise
+        while True:
+            try:    
+                # botao_retorno
+                bot.find_element('//*[@id="form:j_id9"]',
+                                                By.XPATH,
+                                                ensure_clickable=True,
+                                                ensure_visible=True).click()
+                
+                # botao_limpar_digitacao
+                botao_limpar_digitacao = wait.until(
+                    EC.element_to_be_clickable((sBy.XPATH, '//*[@id="form"]/input[3]'))
+                    )
+                bot.wait(500)
+                botao_limpar_digitacao.click()
+                break
+            except ElementClickInterceptedException as err:
+                print('Erro na limpeza dos campos')
+                bot.wait(500)
+                continue
+            except Exception as err:
+                with open(arquivo_progresso, 'w') as f:
+                    f.write(f'Erro após {cliente.ResponsávelFinanceiro} linha {int(cliente.Index) + 1}')
+                    raise err
     sair(bot)
     bot.stop_browser()
 
--- dados.py
+++ dados.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from pathlib import Path
-from tkinter import filedialog
+# from tkinter import filedialog
 
 
 # arquivo_planilha = filedialog.askopenfilename()
-arquivo_planilha = Path(r"C:\Users\novoa\OneDrive\Área de Trabalho\notas_MB\planilhas\zona_norte\escola_canadenseZN_jun24\Maple Bear Jun 24.xlsx")
+arquivo_planilha = Path(r"C:\Users\novoa\OneDrive\Área de Trabalho\notas_MB\planilhas\zona_norte\escola_canadenseZN_ago24\Maple Bear Ago 24.xlsx")
 arquivo_progresso = arquivo_planilha.parent / 'progresso.log'
 arquivo_notas = arquivo_planilha.parent / 'num_notas.txt'
-dados = pd.read_excel(arquivo_planilha, 'dados', header=0)
+dados = pd.read_excel(arquivo_planilha, 'dados', header=1)
 
 dados['Aluno'] = dados['Aluno'].apply(lambda i: i.split()[0])
 
@@ -20,5 +20,6 @@ dados['Alimentação'] = dados['Alimentação'].apply(lambda x: '{:0.2f}'.format
 
 
 if __name__ == '__main__':
-    for i in dados.itertuples():
-        print(i)
+    # for i in dados.itertuples():
+    #     print(i)
+    print(dados)
